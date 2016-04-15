2a4dc86ebbd992d586ba898d8921143f6fe7ef89
asdm <asd@asd.com>
2016-04-15 18:24:20 +0300

    First

---
+++ app/src/main/java/com/knights/agoras/MainActivity.java
@@ -0,0 +1,20 @@
+package com.knights.agoras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLng;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
+
